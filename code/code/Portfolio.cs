3c1f9cdf9dfde2eb5b88e6262219c76335e08a80
Swifteridu <Swifteridu@gmail.com>
2024-09-24 14:44:31 +0200

    Update Portfolio.cs

--- code/code/Portfolio.cs
+++ code/code/Portfolio.cs
@@ -20,49 +20,57 @@ namespace code
         {
             List<WertpapierPosten> liste = new List<WertpapierPosten>();
             string datei = "Personendaten.txt";
-            StreamReader str = new StreamReader(datei);
-            string hallo = "test";
 
-            while (!str.EndOfStream)
+            // Datei öffnen
+            using (StreamReader str = new StreamReader(datei))
             {
-                foreach (string line in str.ReadLine())
+                string line;
+
+                // Datei zeilenweise lesen
+                while ((line = str.ReadLine()) != null)
                 {
                     string[] teil = line.Split(';');
                     string vornameTest = teil[0];
                     string nameTest = teil[1];
+
+                    // Prüfen, ob der Name übereinstimmt
                     if (vorname.Equals(vornameTest) || name.Equals(nameTest))
                     {
-                        
-                        while (hallo != null)
+                        string hallo;
+                        // Weitere Zeilen lesen
+                        while ((hallo = str.ReadLine()) != null && !string.IsNullOrWhiteSpace(hallo))
                         {
-                            hallo = str.ReadLine();
-                            foreach (string line1 in hallo)
+                            string[] teil1 = hallo.Split(';');
+                            int anzahl = int.Parse(teil1[0]);
+                            double wert = double.Parse(teil1[1]);
+                            string typ = teil1[2];
+                            string Tname = teil1[3];
+                            string isinNummer = teil1[4];
+                            // Je nach Wertpapier-Typ handeln
+                            switch (typ)
                             {
-                                string[] teil1 = line1.Split(';');
-                                int anzahl = int.Parse(teil1[0]);
-                                double wert = double.Parse(teil1[1]);
-                                string typ = teil1[2];
-                                string Tname = teil1[3];
-                                string isinNummer = teil1[4];
-
-                                switch (typ)
-                                {
-                                   case "ETF": string basis = teil1[5];
+                                case "ETF":
+                                    string basis = teil1[5];
                                     liste.Add(new WertpapierPosten(anzahl, wert, new ETF(Tname, isinNummer, basis)));
-                                        break;
-                                    case "Optionsschein": DateTime datum = DateTime.Parse(teil1[5]); string Bezeichner = teil1[6];
-                                        liste.Add(new WertpapierPosten(anzahl, wert, new Optionsschein(Tname, isinNummer, datum, Bezeichner)));
-                                        break;
+                                    break;
 
-                                    case "Aktie": string kuerzel = teil1[5]; double devidende = double.Parse(teil1[6]);
-                                        liste.Add(new WertpapierPosten(anzahl, wert, new Aktie(Tname, isinNummer, kuerzel, devidende)));
-                                        break;
-                                    case "Anleihen": DateTime laufzeit = DateTime.Parse(teil1[5]); double koubonwert = double.Parse(teil1[6]);
-                                        liste.Add(new WertpapierPosten(anzahl, wert, new Anleihen(Tname, isinNummer, laufzeit, koubonwert)));
-                                        break;
+                                case "Optionsschein":
+                                    DateTime datum = DateTime.Parse(teil1[5]);
+                                    string Bezeichner = teil1[6];
+                                    liste.Add(new WertpapierPosten(anzahl, wert, new Optionsschein(Tname, isinNummer, datum, Bezeichner)));
+                                    break;
 
-                                }
+                                case "Aktie":
+                                    string kuerzel = teil1[5];
+                                    double devidende = double.Parse(teil1[6]);
+                                    liste.Add(new WertpapierPosten(anzahl, wert, new Aktie(Tname, isinNummer, kuerzel, devidende)));
+                                    break;
 
+                                case "Anleihen":
+                                    DateTime laufzeit = DateTime.Parse(teil1[5]);
+                                    double koubonwert = double.Parse(teil1[6]);
+                                    liste.Add(new WertpapierPosten(anzahl, wert, new Anleihen(Tname, isinNummer, laufzeit, koubonwert)));
+                                    break;
                             }
                         }
                     }
@@ -70,7 +78,5 @@ namespace code
             }
             return liste;
         }
-
-
     }
 }
